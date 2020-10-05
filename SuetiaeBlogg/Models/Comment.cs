1d07603c35ae04a8a058f221f96ef23e6de23abf
NiloufarRashedi <niloufar.rashedi@gmail.com>
2020-10-05 12:59:14 +0200

    Post & Comment models added

---
+++ SuetiaeBlogg/Models/Comment.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SuetiaeBlogg.Models
+{
+    public class Comment
+    {
+        [Required(ErrorMessage = "Name required")]
+        public string AuthorName { get; set; }
+        [Required(ErrorMessage = "Comment text required")]
+        public string Body { get; set; }
+        public DateTimeOffset PubDate { get; set; } = DateTimeOffset.Now;
+        public bool IsPublic { get; set; }
+        public Guid UniqueId { get; set; }
+    }
+}
