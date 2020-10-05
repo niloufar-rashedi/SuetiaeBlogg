b1822b2848376d09959e342d295c63dceda2d0fb
NiloufarRashedi <niloufar.rashedi@gmail.com>
2020-10-05 13:07:57 +0200

    DbContext added

---
+++ SuetiaeBlogg/Data/SuetiaeBloggDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SuetiaeBlogg.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SuetiaeBlogg.Data
+{
+    public class SuetiaeBloggDbContext :DbContext
+    {
+        public SuetiaeBloggDbContext(DbContextOptions<SuetiaeBloggDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+    }
+}
